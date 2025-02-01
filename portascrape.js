91870c4e195f4f779f076511c00250d6a5aef167
ggorlen <gsgorlen@gmail.com>
2025-02-01 13:48:45 -0800

    Fix a few TS issues

--- portascrape.js
+++ portascrape.js
@@ -1,52 +1,47 @@
-/**
- * @typedef {Object} PsOptions
- * @property {number} [timeout=10000] - Timeout in milliseconds. 0 is infinite.
- * @property {"raf" | "mutation" | number} [polling="raf"] - Polling strategy.
- * @property {string} [exactText] - Exact text to match.
- * @property {string} [containsText] - Substring to match.
- * @property {RegExp} [matches] - Regular expression to match.
- */
-
-/**
- * @typedef {Object} Ps
- * @property {function(string, PsOptions): Promise<Element>} $ - Waits for a selector to appear in the DOM and returns the first element that matches.
- * @property {function(string, PsOptions): Promise<Element>} $click - Wait for an element matching selector to exist, then click it.
- * @property {function(string, PsOptions): Promise<string>} $text - Wait for an element matching selector to exist, then extract its `.textContent`.
- * @property {function(string, PsOptions): Promise<void>} $remove - Wait for an element matching selector to exist, then remove it.
- * @property {function(number): Promise<void>} sleep - Sleep for n milliseconds. Discouraged in favor of any of the other operations, but possible.
- * @property {function(string, PsOptions): Promise<string[][]>} $table - Wait for an element to exist, then scrape its `<tr>`, `<th>` and `<td>` content into a 2d array.
- * @property {function(string, PsOptions): Promise<Object<string, string>[]>} $tableWithHeaders - Wait for an element to exist, then scrape its `<tr>`, `<th>` and `<td>` content into an array of objects.
- * @property {function(function(): boolean, PsOptions): Promise<void>} wait - Waits for an arbitrary predicate function to return truthy.
- */
-
-/**
- * Default ps options.
- * @typedef {Object} DefaultPsOptions
- * @property {number} [timeout=10000] - Timeout in milliseconds. 0 is infinite.
- * @property {"raf" | "mutation" | number} [polling="raf"] - Polling strategy.
- */
-
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD support
     define([], factory);
   } else if (typeof module === "object" && module.exports) {
-    // CommonJS/Node.js support
     module.exports = factory();
   } else {
     // Browser global
-    root.ps = factory();
+    /** @type {any} */ (root).ps = factory();
   }
 })(typeof self !== "undefined" ? self : this, function () {
   /**
-   * @type {DefaultPsOptions}
+   * @typedef {Object} PsOptions
+   * @property {number} [timeout=10000] - Timeout in milliseconds. 0 is infinite.
+   * @property {"raf" | "mutation" | number} [polling="raf"] - Polling strategy.
+   * @property {string} [exactText] - Exact text to match.
+   * @property {string} [containsText] - Substring to match.
+   * @property {RegExp} [matches] - Regular expression to match.
+   */
+
+  /**
+   * @typedef {Object} Ps
+   * @property {function(string, PsOptions | undefined): Promise<Element>} $ - Waits for a selector to appear in the DOM and returns the first element that matches.
+   * @property {function(string, PsOptions): Promise<Element>} $click - Wait for an element matching selector to exist, then click it.
+   * @property {function(string, PsOptions): Promise<string | null>} $text - Wait for an element matching selector to exist, then extract its `.textContent`.
+   * @property {function(string, PsOptions): Promise<void>} $remove - Wait for an element matching selector to exist, then remove it.
+   * @property {function(number): Promise<void>} sleep - Sleep for n milliseconds. Discouraged in favor of any of the other operations, but possible.
+   * @property {function(string, PsOptions): Promise<(string | null)[][]>} $table - Wait for an element to exist, then scrape its `<tr>`, `<th>` and `<td>` content into a 2d array.
+   * @property {function(string, PsOptions): Promise<Object<string, string | null>[]>} $tableWithHeaders - Wait for an element to exist, then scrape its `<tr>`, `<th>` and `<td>` content into an array of objects.
+   * @property {function(function(): boolean, PsOptions): Promise<void>} wait - Waits for an arbitrary predicate function to return truthy.
+   */
+
+  /**
+   * Default ps options.
+   * @type {Object} DefaultPsOptions
+   * @property {number} [timeout=10000] - Timeout in milliseconds. 0 is infinite.
+   * @property {"raf" | "mutation" | number} [polling="raf"] - Polling strategy.
    */
   var defaultOptions = Object.freeze({ timeout: 10000, polling: "raf" });
 
   /**
    * Waits for an arbitrary predicate function to return truthy.
-   * @param {function(): boolean} fn - The predicate function to wait for.
-   * @param {PsOptions} [options] - The options for waiting.
+   * @param {function(): any} fn - The predicate function to wait for.
+   * @param {PsOptions | undefined} [options] - The options for waiting.
    * @returns {Promise<any>} A promise that resolves when the predicate returns truthy.
    */
   function wait(fn, options) {
@@ -60,15 +55,17 @@
         return resolve(result);
       }
 
+      /** @type {ReturnType<typeof setTimeout>} */
       var timeoutId;
 
-      if (options.timeout > 0) {
+      if (options.timeout && options.timeout > 0) {
         timeoutId = setTimeout(function () {
-          reject(
+          var error = new Error(
             "timeout of " +
               options.timeout +
-              "ms exceeded waiting for function to return true",
+              "ms exceeded waiting for function to return true"
           );
+          reject(error);
         }, options.timeout);
       }
 
@@ -94,7 +91,7 @@
           return resolve(result);
         }
 
-        if (typeof options.polling === "number") {
+        if (options && typeof options.polling === "number") {
           setTimeout(poll, options.polling);
         } else {
           requestAnimationFrame(poll);
@@ -116,7 +113,14 @@
     return ps.$(selector, options).then(function (element) {
       return new Promise(function (resolve, reject) {
         try {
-          element.click();
+          if (element instanceof HTMLElement) {
+            element.click();
+          } else {
+            throw Error(
+              "Element of type " + typeof element + " is unclickable"
+            );
+          }
+
           resolve(element);
         } catch (error) {
           reject(error);
@@ -130,7 +134,7 @@
    *
    * @param {string} selector - The selector to wait for.
    * @param {PsOptions} [options] - The options for waiting and selecting.
-   * @returns {Promise<string>} A promise that resolves with the textContent of the element.
+   * @returns {Promise<string | null>} A promise that resolves with the textContent of the element.
    */
   function $text(selector, options) {
     return ps.$(selector, options).then(function (element) {
@@ -163,14 +167,14 @@
    *
    * @param {string} selector - The selector to wait for.
    * @param {PsOptions} [options] - The options for waiting and selecting.
-   * @returns {Promise<Array<Array<string>>>} A promise that resolves with the table contents.
+   * @returns {Promise<Array<Array<string | null>>>} A promise that resolves with the table contents.
    */
   function $table(selector, options) {
     return ps.$(selector, options).then(function (tableElement) {
       var rows = Array.from(tableElement.querySelectorAll("tr"));
       var tableData = rows.map(function (row) {
         return Array.from(row.querySelectorAll("td, th")).map(function (cell) {
-          return cell.textContent.trim();
+          return cell.textContent && cell.textContent.trim();
         });
       });
       return tableData;
@@ -182,26 +186,30 @@
    *
    * @param {string} selector - The selector to wait for.
    * @param {PsOptions} [options] - The options for waiting and selecting.
-   * @returns {Promise<Array<Object>>} A promise that resolves with the table contents.
+   * @returns {Promise<Array<Record<string, string | null>>>} A promise that resolves with the table contents.
    */
   function $tableWithHeaders(selector, options) {
     return ps.$(selector, options).then(function (tableElement) {
       var rows = Array.from(tableElement.querySelectorAll("tr"));
-      var headers = Array.from(rows.shift().querySelectorAll("th")).map(
+
+      var headers = Array.from(rows[0].querySelectorAll("th")).map(
         function (header) {
-          return header.textContent.trim();
-        },
+          return header.textContent && header.textContent.trim();
+        }
       );
 
-      var tableData = rows.map(function (row) {
+      var tableData = rows.slice(1).map(function (row) {
         var rowData = Array.from(row.querySelectorAll("td")).map(
           function (cell) {
-            return cell.textContent.trim();
-          },
+            return cell.textContent ? cell.textContent.trim() : null;
+          }
         );
+        /** @type Record<string, string | null> */
         var rowObject = {};
         headers.forEach(function (header, index) {
-          rowObject[header] = rowData[index];
+          if (header !== null) {
+            rowObject[header] = rowData[index];
+          }
         });
         return rowObject;
       });
@@ -229,6 +237,14 @@
    * @returns {Promise<Element>} A promise that resolves with the found element.
    */
   function $(selector, options) {
+    /**
+     * Loops over childNodes of an element to find a text node
+     * that fulfills an arbitrary predicate.
+     *
+     * @param {Element} element - The element to search through.
+     * @param {function(ChildNode): any} predicate - The predicate to fulfill.
+     * @returns {Element?} The element, if found.
+     */
     function search(element, predicate) {
       for (var j = 0; j < element.childNodes.length; j++) {
         var child = element.childNodes[j];
@@ -237,9 +253,11 @@
           return element;
         }
       }
+
+      return null;
     }
 
-    return wait(() => {
+    return wait(function () {
       if (
         !options ||
         (!options.exactText && !options.containsText && !options.matches)
@@ -255,19 +273,30 @@
 
         if (options.exactText || options.containsText) {
           result = search(element, function (child) {
-            return child.textContent.trim() === options.exactText;
+            return (
+              child.textContent &&
+              child.textContent.trim() === options.exactText
+            );
           });
         }
 
         if (options.containsText) {
           result = search(element, function (child) {
-            return child.textContent.indexOf(options.containsText) > -1;
+            return (
+              child.textContent &&
+              options.containsText &&
+              child.textContent.indexOf(options.containsText) > -1
+            );
           });
         }
 
         if (options.matches) {
           result = search(element, function (child) {
-            return options.matches.test(child.textContent.trim());
+            return (
+              child.textContent &&
+              options.matches &&
+              options.matches.test(child.textContent.trim())
+            );
           });
         }
 
@@ -282,7 +311,7 @@
 
   /**
    * Validates the options object for ps functions.
-   * @param {PsOptions} options - The options to validate.
+   * @param {any} options - The options to validate.
    * @throws {Error} Throws an error if options are invalid or conflicting.
    */
   function validateOptions(options) {
@@ -297,11 +326,11 @@
     var providedTextOptions = 0;
 
     for (var key in options) {
-      if (!validKeys.includes(key)) {
+      if (validKeys.indexOf(key) >= 0) {
         throw new Error("Invalid option '" + key + "' provided.");
       }
 
-      if (textOptions.includes(key)) {
+      if (textOptions.indexOf(key) >= 0) {
         if (/text/.test(key) && typeof options[key] !== "string") {
           throw new TypeError("'" + key + "' must be a string.");
         }
@@ -315,7 +344,7 @@
 
       if (providedTextOptions > 1) {
         throw new Error(
-          "Only one of 'exactText', 'containsText', or 'matches' can be provided.",
+          "Only one of 'exactText', 'containsText', or 'matches' can be provided."
         );
       }
     }
